1bce4100e1b15f028c70e337420cb441e9804610
Drew Higgins <me@drewhiggins.com>
2026-06-27 11:16:59 -0700

    Bump version to 1.0.0

--- src/SyncTray/Constants.cs
+++ src/SyncTray/Constants.cs
@@ -4,7 +4,7 @@ public static class Constants
 {
     public const string SyncthingExePath = @"C:\Program Files\Syncthing\syncthing.exe";
     public const int PollIntervalSeconds = 20;
-    public const string Version = "v0.0.1-int0";
+    public const string Version = "v1.0.0";
     public const string AppName = "SyncTray";
     public const string DefaultHost = "localhost";
     public const int DefaultPort = 8384;
